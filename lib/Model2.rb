7480b659fcbcb797245bc7809249a02a6767acd7
github username <github email address>
2019-10-14 16:03:04 -0400

    Done.

--- lib/Model2.rb
+++ lib/Model2.rb
@@ -1,5 +1,17 @@
 #class for Model2 goes here
 #Feel free to change the name of the class
-class Model2
+class Position
+    attr_reader :name, :player, :team
+    @@all =[]
+    def initialize(name,player,team)
+        @name=name
+        @team=team
+        @player=player
+        @@all<<self
+    end
+    def self.all
+        @@all
+    end
+
 
 end
--- lib/Model3.rb
+++ lib/Model3.rb
@@ -1,5 +1,25 @@
 #class for Model3 goes here
 #Feel free to change the name of the class
-class Model3
-  
+class Player
+    attr_reader :name, :college
+    @@all =[]
+    def initialize(name,college)
+        @name=name
+        @college=college
+        @@all << self
+    end
+    def position
+        Position.all.select do |position|
+            position.player == self
+        end
+    end
+    def team
+        team=position.map do |pos|
+            pos.team
+        end
+        team[0]
+    end
+    def self.all
+        @@all
+    end
 end
